bc841f9331a8ecc59160d19f6ae80d5d0dcf3548
jcass <john.cass77@gmail.com>
2016-09-19 19:01:59 +0200

    Fix field name reference.

--- flask_stormpath/forms.py
+++ flask_stormpath/forms.py
@@ -101,7 +101,7 @@ class ChangePasswordForm(Form):
     password = PasswordField('Password', validators=[InputRequired('Password required.')])
     password_again = PasswordField('Password (again)', validators=[
         InputRequired('Please verify the password.'),
-        EqualTo(password, 'Passwords don\'t match.')
+        EqualTo('password', 'Passwords do not match.')
     ])
 
 class ChangePasswordFormHref(ChangePasswordForm):
